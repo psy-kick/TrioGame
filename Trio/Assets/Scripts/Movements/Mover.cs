6b9af6338f67d1c18097813d67328a0645cbea79
Suhas Aditya SM <suhasadityasm@gmail.com>
2024-06-04 16:35:06 +0530

    basic input system and movements done

--- Trio/Assets/Scripts/Movements/Mover.cs
+++ Trio/Assets/Scripts/Movements/Mover.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class Mover : MonoBehaviour
 {
     private InputActions inputActions;
@@ -17,7 +16,11 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private LayerMask GroundLayer;
     [SerializeField]
-    private bool isGrounded;
+    private bool isGrounded = false;
+    [SerializeField]
+    private float GroundCheckRadius;
+    private bool isFacingRight = true;
+    private Vector2 InputAxis;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,32 +36,42 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isFacingRight && InputAxis.x > 0)
+        {
+            Flip();
+        }
+        else if(isFacingRight && InputAxis.x < 0)
+        {
+            Flip();
+        }
         GroundChecker();
     }
     private void FixedUpdate()
     {
-        Move();
         Jump();
+        Move();
     }
     private void Move()
     {
-        Vector2 InputAxis = inputActions.Player.Mover.ReadValue<Vector2>();
-        rb.velocity = new Vector2(InputAxis.x, 0f) * MoveSpeed;
+        InputAxis = inputActions.Player.Mover.ReadValue<Vector2>();
+        rb.velocity = new Vector2(InputAxis.x * MoveSpeed, rb.velocity.y);
     }
     private void Jump()
     {
         if (inputActions.Player.Jump.IsPressed() && isGrounded)
         {
-            rb.AddForce(Vector2.up * JumpHeight, ForceMode2D.Impulse);
-        }
-        else
-        {
-            isGrounded = false;
-        }
-        
+            rb.velocity = new Vector2(rb.velocity.x, JumpHeight);
+        }        
     }
     private void GroundChecker()
     {
-        isGrounded = Physics2D.Raycast(Groundcheck.position, Vector2.down * 0.1f, GroundLayer);
+        isGrounded = Physics2D.OverlapCircle(Groundcheck.position, GroundCheckRadius, GroundLayer);
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 LocalScale = transform.localScale;
+        LocalScale.x *= -1f;
+        transform.localScale = LocalScale;
     }
 }
